cb8bb4f601938922b581f013f97acf441a7be007
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-02 15:04:18 +0300

    Create patients model

---
+++ app/Models/Patients.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Patients extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = "esite_patients";
+}
