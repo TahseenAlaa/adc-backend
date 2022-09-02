e3cc3175a7fbadfca7141d9e654ae1282a43e75f
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-02 15:51:49 +0300

    Add field to the patients resource

--- app/Http/Resources/PatientsResource.php
+++ app/Http/Resources/PatientsResource.php
@@ -15,6 +15,7 @@ class PatientsResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'full_name'    => $this->full_name,
             'phone'    => $this->phone,
             'occupation'    => $this->occupation,
