96d48d9b2b900c2c067a68cc5f529600f9364a6e
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-11 18:35:36 +0300

    Create doctors migration

---
+++ database/migrations/2022_09_11_153247_create_esite_doctors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('esite_doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('occupation')->nullable();
+            $table->dateTime('birthday')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('esite_doctors');
+    }
+};
