c3903d6b24e5d0bb9ee15bcea5ee3b4aaefef2c7
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-11 18:39:45 +0300

    Create diagnosis migration

---
+++ database/migrations/2022_09_11_153646_create_esite_diagnosis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('esite_diagnosis', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('is_confirmed')->comment('0 -> suspected, 1 -> confirmed'); // 0 -> suspected, 1 -> confirmed
+            $table->integer('doctor_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('esite_diagnosis');
+    }
+};
