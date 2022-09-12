beac85b6f544b65ae2e3f899c4ab578df5927ad0
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-12 06:53:23 +0300

    Add api token support to the models

--- app/Models/Patients.php
+++ app/Models/Patients.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
 
 class Patients extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasApiTokens, HasFactory, SoftDeletes;
 
     protected $table = "esite_patients";
 
