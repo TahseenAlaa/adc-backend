90445b5f4a7babce40bd2eebf9cf2b59aee01cb8
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-12 06:18:33 +0300

    Create pharmacy migration

---
+++ database/migrations/2022_09_12_031512_create_esite_pharmacy_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('esite_pharmacy', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('batch_no')->nullable();
+            $table->date('expire_date');
+            $table->string('treatment_type')->nullable();
+            $table->string('dosage')->nullable();
+            $table->integer('quantity');
+            $table->longText('notes');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('esite_pharmacy');
+    }
+};
