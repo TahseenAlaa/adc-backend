47e029c2ed5cbbf34a34246582e3d786ff56c764
TahseenAlaa <raspoteen7@gmail.com>
2022-09-13 12:43:45 +0300

    Fix bug

--- database/migrations/2022_09_11_154001_create_esite_treatment_table.php
+++ database/migrations/2022_09_11_154001_create_esite_treatment_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('dose')->nullable();
             $table->string('status')->nullable();
             $table->integer('created_by');
-            $table->integer('updated_by');
+            $table->integer('updated_by')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
