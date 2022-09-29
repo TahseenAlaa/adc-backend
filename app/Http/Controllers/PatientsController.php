e31038383d48f2ef005867b640baa8da66b51ba3
Tahseen Alaa <raspoteen7@gmail.com>
2022-09-30 00:17:45 +0300

    Store patient data related to doctor

--- app/Http/Controllers/PatientsController.php
+++ app/Http/Controllers/PatientsController.php
@@ -340,4 +340,29 @@ class PatientsController extends Controller
             'data' => $PatientInfo
         ]);
     }
+
+    public function storePatientInfoByDr(Request $r) {
+        $patientId = Patients::select('id')->where('uuid', '=', $r->patient_uuid)->first();
+
+        PatientsHistory::where('patient_id', '=', $patientId->id)
+            ->orderBy('id','desc')
+            ->take(1)
+            ->update([
+            'patient_number'            => $r->patient_number,
+            'age_at_visit'              => $r->age_at_visit,
+            'blood_pressure_systolic'   => $r->blood_pressure_systolic,
+            'blood_pressure_diastolic'  => $r->blood_pressure_diastolic,
+            'weight_by_dr'              => $r->weight_by_dr,
+            'height_by_dr'              => $r->height_by_dr,
+            'waist_circumference_by_dr' => $r->waist_circumference_by_dr,
+            'bmi_by_dr'                 => $r->bmi_by_dr,
+            'clinical_notes'            => $r->clinical_notes,
+            'next_visit'                => $r->next_visit,
+            'created_by_dr'             => auth('sanctum')->user()->id
+        ]);
+
+        return response([
+            'date' => 'Save Successfully!'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ use App\Http\Controllers\DashboardController;
 Route::prefix('/v1')->name('api.v1.')->group(function () {
     // START Auth
     Route::prefix('/auth')->name('auth.')->group(function () {
-        Route::post('/signup', [AuthController::class, 'signup'])->middleware('auth:sanctum')->name('signup');
+        Route::post('/signup', [AuthController::class, 'signup'])->name('signup');
         Route::post('/login', [AuthController::class, 'login'])->name('login');
         Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
         Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -51,6 +51,7 @@ Route::prefix('/v1')->name('api.v1.')->group(function () {
         Route::get('/search-by-full-name/{name}', [PatientsController::class, 'searchByFullName'])->name('searchByFullName');
         Route::get('/search-by-phone/{phone}', [PatientsController::class, 'searchByPhone'])->name('searchByPhone');
         Route::get('/search-by-patient-id/{patient}', [PatientsController::class, 'searchByPatientID'])->name('searchByPatientID');
+        Route::post('/store-by-dr', [PatientsController::class, 'storePatientInfoByDr'])->name('storeByDoctor');
     });
     // END Patients
 
