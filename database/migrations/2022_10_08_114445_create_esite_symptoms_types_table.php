e07004d99a09b7b3d58d27e978a31b11b17d32fa
Tahseen Alaa <raspoteen7@gmail.com>
2022-10-10 19:57:27 +0300

    Fix bugin migration

--- database/migrations/2022_10_08_114445_create_esite_symptoms_types_table.php
+++ database/migrations/2022_10_08_114445_create_esite_symptoms_types_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->integer('updated_by')->nullable();
             $table->timestamps();
             $table->softDeletes();
-            $table->timestamps();
         });
     }
 
