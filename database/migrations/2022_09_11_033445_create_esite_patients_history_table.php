08e4a7734cf21b5a5916144081407abcd36d3925
TahseenAlaa <raspoteen7@gmail.com>
2022-10-15 19:22:13 +0300

    Update patient history migration table

--- database/migrations/2022_09_11_033445_create_esite_patients_history_table.php
+++ database/migrations/2022_09_11_033445_create_esite_patients_history_table.php
@@ -17,32 +17,23 @@ return new class extends Migration
             $table->id();
             $table->uuid('uuid')->unique();
             $table->integer('patient_id');
-            $table->string('patient_picture_id')->nullable();
-            $table->date('date_of_visit')->nullable();
-            $table->integer('blood_pressure_systolic')->nullable();
-            $table->integer('blood_pressure_diastolic')->nullable();
             $table->integer('weight')->nullable();
             $table->integer('height')->nullable();
             $table->integer('waist_circumference')->nullable();
+            $table->integer('hip')->nullable();
             $table->integer('bmi')->nullable();
+            $table->integer('father_height')->nullable();
+            $table->integer('mother_height')->nullable();
+            $table->integer('mid_height')->nullable();
+            $table->string('gender')->nullable();
+            $table->integer('blood_pressure_systolic')->nullable();
+            $table->integer('blood_pressure_diastolic')->nullable();
             $table->integer('age_at_visit')->nullable();
-            $table->longText('notes')->nullable();
             $table->date('next_visit')->nullable();
             $table->integer('created_by');
             $table->integer('updated_by')->nullable();
             $table->timestamps();
             $table->softDeletes();
-
-
-//            $table->string('gender')->nullable();
-//            $table->integer('weight')->nullable();
-//            $table->integer('height')->nullable();
-//            $table->integer('waist_circumference')->nullable();
-//            $table->integer('bmi')->nullable();
-//            $table->string('hip')->nullable();
-//            $table->integer('father_height')->nullable();
-//            $table->integer('mother_height')->nullable();
-//            $table->integer('mid_height')->nullable();
 
         });
     }
