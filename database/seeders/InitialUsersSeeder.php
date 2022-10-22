25fe22a6cddac7e7085dbf4bd67efa119feda2e2
TahseenAlaa <raspoteen7@gmail.com>
2022-10-22 21:36:32 +0300

    Fix bug

--- database/seeders/InitialUsersSeeder.php
+++ database/seeders/InitialUsersSeeder.php
@@ -23,7 +23,7 @@ class InitialUsersSeeder extends Seeder
             'username'        => 'areeb',
             'job_title'       => 'Web Developer',
             'created_by'      => 1,
-            'last_login_ip'   => fake()->ipv4(),
+            'last_login_ip'   => '127.0.0.1',
             'last_login_at'   => Carbon::now(),
             'password'        => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token'  => Str::random(10),
