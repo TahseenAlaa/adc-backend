2d00e8417ed02d10604f573a06e391bde4ae7115
TahseenAlaa <raspoteen7@gmail.com>
2022-10-23 04:03:39 +0300

    Add lab sampling to the router api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\SymptomsTypesController;
 use App\Http\Controllers\SymptomsController;
 use App\Http\Controllers\LabTestGroupsController;
 use App\Http\Controllers\AnthoController;
+use App\Http\Controllers\LabSamplingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -100,6 +101,12 @@ Route::prefix('/v1')->name('api.v1.')->group(function () {
         Route::get('/show-history/{id}', [MedicalLabController::class, 'showHistory'])->name('history.show');
     });
     // END Medical Lab
+
+    // START Lab Sampling
+    Route::prefix('/lab-sampling')->name('lab_sampling.')->middleware('auth:sanctum')->group(function () {
+        Route::post('/store', [LabSamplingController::class, 'store'])->name('store');
+    });
+    // END Lab Sampling
 
     // START Lab Test Groups
     Route::prefix('/lab-test-groups')->name('test-groups.')->middleware('auth:sanctum')->group(function () {
