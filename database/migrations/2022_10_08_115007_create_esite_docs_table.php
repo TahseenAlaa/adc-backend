4967543588933ff523c9bc131980fbdb6ec64aff
TahseenAlaa <raspoteen7@gmail.com>
2022-11-02 11:35:09 +0300

    Update documents migration table

--- database/migrations/2022_10_08_115007_create_esite_docs_table.php
+++ database/migrations/2022_10_08_115007_create_esite_docs_table.php
@@ -16,15 +16,15 @@ return new class extends Migration
         Schema::create('esite_docs', function (Blueprint $table) {
             $table->id();
             $table->integer('provider_id');
-            $table->integer('doc_number')->nullable();
-            $table->date('doc_date')->nullable();
+            $table->string('source_ref')->nullable();
+            $table->string('source_name')->nullable();
+            $table->string('source_job_title')->nullable();
+            $table->string('destination_ref')->nullable();
+            $table->string('destination_name')->nullable();
+            $table->string('destination_job_title')->nullable();
             $table->integer('doc_type')->comment('1: Input Doc, 2: Output Doc.')->nullable();
             $table->boolean('to_pharmacy')->comment('1: True, 0: False')->nullable();
-            $table->string('to_name')->nullable();
-            $table->string('sender_name')->nullable();
-            $table->string('sender_job_title')->nullable();
-            $table->string('receiver_name')->nullable();
-            $table->string('receiver_job_title')->nullable();
+            $table->boolean('final_approval')->comment('1: Yes, 0: No')->nullable(); // 1: Yes, 0: No
             $table->string('manager_name')->nullable();
             $table->integer('approved_by')->nullable();
             $table->dateTime('approved_at')->nullable();
