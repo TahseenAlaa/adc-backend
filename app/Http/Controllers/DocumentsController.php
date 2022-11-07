649912ca9eae7ada132c92d52790ad771705b9a7
TahseenAlaa <raspoteen7@gmail.com>
2022-11-07 04:24:20 +0300

    Fix bug in the inventory

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -82,15 +82,16 @@ class DocumentsController extends Controller
         if ($request->doc_type === 1) { // Input Document
             $newDocument->destination_id = Providers::select('id')->where('title', '=', 'Alhasan Diabetes Center')->first()->id;
             $newDocument->provider_id = $request->provider_id;
+            $newDocument->to_pharmacy = false;
         } else if ($request->doc_type === 2) { // Output Document
             $newDocument->destination_id = $request->destination_id;
             $newDocument->provider_id = Providers::select('id')->where('title', '=', 'Alhasan Diabetes Center')->first()->id;
+            $newDocument->to_pharmacy = $request->to_pharmacy;
         }
         $newDocument->destination_name = $request->destination_name;
         $newDocument->destination_ref = $request->destination_reference;
         $newDocument->destination_job_title = $request->destination_job_title;
         $newDocument->doc_type = $request->doc_type; // 1: Input Doc, 2: Output Doc.
-        $newDocument->to_pharmacy = $request->to_pharmacy;
         $newDocument->final_approval = $request->final_approval;
         $newDocument->approved_by = $request->approved_by;
         $newDocument->approved_at = $request->approved_at;
@@ -108,11 +109,12 @@ class DocumentsController extends Controller
             $newDocumentItem->batch_no = $item['batch'];
             $newDocumentItem->expire_date = $item['expire_date'];
             $newDocumentItem->doc_type = $request->doc_type;
-            $newDocumentItem->to_pharmacy = $request->to_pharmacy;
             if ($request->doc_type === 2) { /* Output Document */ // TODO Validation Calc_quantity >= 1
                 $newDocumentItem->calc_quantity = $request->calc_quantity;
-            } else if ($request->doc_type === 1) {
+                $newDocumentItem->to_pharmacy = $request->to_pharmacy;
+            } else if ($request->doc_type === 1) { // Input Document
                 $newDocumentItem->calc_quantity = $item['quantity'];
+                $newDocumentItem->to_pharmacy = false;
             }
             $newDocumentItem->created_by = auth('sanctum')->user()->id;
             $newDocumentItem->created_at = Carbon::now();
