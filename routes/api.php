34d44c826a23d9a6c8bc4c9b9e0c2dd8f4773820
TahseenAlaa <raspoteen7@gmail.com>
2022-11-30 05:48:00 +0300

    Fix bug

--- routes/api.php
+++ routes/api.php
@@ -212,8 +212,8 @@ Route::prefix('/v1')->name('api.v1.')->group(function () {
         Route::get('/index-inventory', [DocumentsController::class, 'indexInventory'])->name('index-inventory');
         Route::get('/available-drugs', [DocumentsController::class, 'indexAvailableDrugs'])->name('available-drugs');
         Route::get('/output-document', [DocumentsController::class, 'indexOutputDocuments'])->name('output-documents');
-        Route::post('/delete-input-document', [DocumentsController::class, 'destroyInputDocument'])->name('delete');
-        Route::post('/delete-output-document', [DocumentsController::class, 'destroyOutputDocument'])->name('delete');
+        Route::post('/delete-input-document', [DocumentsController::class, 'destroyInputDocument'])->name('delete-input-document');
+        Route::post('/delete-output-document', [DocumentsController::class, 'destroyOutputDocument'])->name('delete-output-document');
         Route::post('/show-input-document', [DocumentsController::class, 'showInputDocument'])->name('show-input-document');
         Route::post('/show-output-document', [DocumentsController::class, 'showOutputDocument'])->name('show-output-document');
     });
