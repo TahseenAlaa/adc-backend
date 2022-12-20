55b68609739083a90999000701f1f6963409c0b0
TahseenAlaa <raspoteen7@gmail.com>
2022-12-20 04:04:47 +0300

    Create committee approvals table with migration

---
+++ database/migrations/2022_12_20_040002_create_esite_committee_approvals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('esite_committee_approvals', function (Blueprint $table) {
+            $table->id();
+            $table->integer('status')->comment('1: Approved, 0: Rejected');
+            $table->integer('created_by');
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('esite_committee_approvals');
+    }
+};
